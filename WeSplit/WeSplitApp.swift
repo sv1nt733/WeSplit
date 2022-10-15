548f0f55a7141bb33ba473a060c27ccf563b6fc8
Dillon Waugh <waughdillon66@gmail.com>
2022-10-15 02:19:14 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Dillon Waugh on 15/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
